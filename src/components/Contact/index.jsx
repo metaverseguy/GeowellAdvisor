f2e618fa5f96b05d59636a03385c0c781d30ced6
AcePatterson <acepttrsn@gmail.com>
2023-09-26 00:40:29 +0530

    fix: app-link

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -23,7 +23,7 @@ function Contact() {
           <div className='contact-container' data-aos="fade-in" data-aos-delay="1000">
               <Card name="Tanishq Agarwal" title="Front-End Developer" image="https://media.licdn.com/dms/image/D4D03AQErSFQvvNkzmA/profile-displayphoto-shrink_800_800/0/1691864426120?e=1701302400&v=beta&t=lnPeW4M6mbORNwgcYZXLW1ABT9y6oaklbUCmaKhPmxc" />
               <Card name="Ayushi Dwivedi" title="ML Developer" image="https://media.licdn.com/dms/image/D4D03AQHVTV78NLknYg/profile-displayphoto-shrink_800_800/0/1692426271520?e=1701302400&v=beta&t=I7XJI1KBGO4B9MWzvtKTvdWkF4cEI3VXSQDJtKdQcaw"/>
-              <Card name="Satyam Gupta" title="Front-End Developer" image="https://i.ibb.co/DbqxFmG/profile-photo.jpg"/>
+              <Card name="Satyam Gupta" title="Front-End Developer" image="https://i.postimg.cc/cJtN25jq/Whats-App-Image-2023-09-17-at-23-53-53.jpg"/>
           </div>
           <div className='contact-container' data-aos="fade-in" data-aos-delay="2000">
               <Card name="Satyam Singh" title="Front-End Developer" image="https://media.licdn.com/dms/image/D4D03AQHjyLJxN6LLZw/profile-displayphoto-shrink_800_800/0/1686240990025?e=1701302400&v=beta&t=sTTrISVOArn3QGzr_SwURpInpKOvMOTQcLgv7LqrSxk"/>
